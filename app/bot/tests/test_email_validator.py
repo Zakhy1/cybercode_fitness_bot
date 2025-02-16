dc759412ea99ea56ae4598f1f21bb4f23a39f2f5
zakhy <lirchzakhar@gmail.com>
2025-02-16 17:03:50 +0700

    переезд на pytest

--- app/bot/tests/test_email_validator.py
+++ app/bot/tests/test_email_validator.py
@@ -3,36 +3,32 @@ import unittest
 from bot.validators.is_corporate_email import is_corporate_email
 
 
-class CorporateEmailTestCase(unittest.TestCase):
+class TestCorporateEmail:
     def test_correct(self):
         value = is_corporate_email("z.lyrshikov@cybercode.pro")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = is_corporate_email("a.pichugin@cybercode.pro")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = is_corporate_email("d.krivosheev@cybercode.pro")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = is_corporate_email("d.shibert@cybercode.pro")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = is_corporate_email("r.gartman@cybercode.pro")
-        self.assertEqual(value, True)
+        assert value is True
 
     def test_incorrect(self):
         value = is_corporate_email("z.lyrshikov@cyberocode.pro")
-        self.assertEqual(value, False)
+        assert value is False
 
         value = is_corporate_email("@cybercode.pro")
-        self.assertEqual(value, False)
+        assert value is False
 
         value = is_corporate_email("d.krivosheev@gmail.pro")
-        self.assertEqual(value, False)
+        assert value is False
 
         value = is_corporate_email("d.shibert@cybercode")
-        self.assertEqual(value, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert value is False
--- app/bot/tests/test_name_validator.py
+++ app/bot/tests/test_name_validator.py
@@ -1,25 +1,23 @@
-import unittest
-
 from bot.validators.validate_name import validate_name
 
 
-class UserNameTestCase(unittest.TestCase):
+class TestUserName:
     def test_correct(self):
         value = validate_name("Иванов Иван Иванович")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = validate_name("Иванов Иван")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = validate_name("Иванов Иван Иванович")
-        self.assertEqual(value, True)
+        assert value is True
 
         value = validate_name("Иванов-Петров Иван")
-        self.assertEqual(value, True)
+        assert value is True
 
     def test_incorrect(self):
         value = validate_name("Иванов")
-        self.assertEqual(value, False)
+        assert value is False
 
         value = validate_name("Иванов Иван Иван Иван")
-        self.assertEqual(value, False)
+        assert value is False

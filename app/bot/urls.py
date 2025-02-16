b3b3a3a220e3c8f02e449e02220b504878c5bc52
zakhy <lirchzakhar@gmail.com>
2025-02-16 14:51:46 +0700

    классификация всего

--- app/bot/urls.py
+++ app/bot/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('getpost/', views.telegram_bot, name='telegram_bot'),
-    path('setwebhook/', views.setwebhook, name='setwebhook')
+    path('getpost/', views.TelegramBotWebhookView.as_view(),
+         name='telegram_bot'),
+    path('setwebhook/', views.TelegramBotSetwebhookView.as_view(),
+         name='setwebhook')
 ]
--- app/bot/views.py
+++ app/bot/views.py
@@ -2,46 +2,44 @@ import json
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
 from bot.core.message_dispatcher import TelegramBotDispatcher
+from bot.util.csrf_exempt_mixin import CSRFExemptMixin
 from project.settings import TELEGRAM_API_URL
 from project.logging_settings import error_logger, info_logger
 
 from settings.models import Settings
 
-from bot.core.base import handle_callback_query
-from bot.core.message_handler import TelegramBotHandler
-from bot.tasks import send_message
-from bot.util.base_view import base_view
-
 import requests
 
 
-@base_view
-@csrf_exempt
-def telegram_bot(request):
-    if request.method == 'POST':
-        message = {}
+class TelegramBotWebhookView(CSRFExemptMixin, View):
+    def post(self, request):
+        if request.method == 'POST':
+            message = {}
+            try:
+                message = json.loads(request.body.decode('utf-8'))
+                dispatcher = TelegramBotDispatcher()
+                dispatcher.dispatch(message)
+            except Exception as e:
+                error_logger.error(
+                    f"Ошибка во время обработки сообщения: {e}\n\n "
+                    f"Сообщение: {message}\n")
+                return HttpResponse('error', status=500)
+
+        return HttpResponse('ok')
+
+
+class TelegramBotSetwebhookView(View):
+    def get(self, request):
         try:
-            message = json.loads(request.body.decode('utf-8'))
-            dispatcher = TelegramBotDispatcher()
-            dispatcher.dispatch(message)
+            telegram_token = Settings.get_setting("TELEGRAM_TOKEN")
+            host_url = Settings.get_setting("HOST_URL") + '/getpost/'
+            url = TELEGRAM_API_URL + telegram_token + "/setWebhook?url=" + host_url
+            response = requests.post(url).json()
+            info_logger.info(f"Установка webhook на url ({url})")
+            info_logger.info(f"{response}")
+            return HttpResponse(f"{response}")
         except Exception as e:
-            error_logger.error(f"Ошибка во время обработки сообщения: {e}\n\n "
-                               f"Сообщение: {message}\n")
-            return HttpResponse('error', status=500)
-
-    return HttpResponse('ok')
-
-
-def setwebhook(request):
-    try:
-        telegram_token = Settings.get_setting("TELEGRAM_TOKEN")
-        host_url = Settings.get_setting("HOST_URL") + '/getpost/'
-        url = TELEGRAM_API_URL + telegram_token + "/setWebhook?url=" + host_url
-        response = requests.post(url).json()
-        info_logger.info(f"Установка webhook на url ({url})")
-        info_logger.info(f"{response}")
-        return HttpResponse(f"{response}")
-    except Exception as e:
-        return HttpResponse(f"Not set: {str(e)}")
+            return HttpResponse(f"Not set: {str(e)}")
